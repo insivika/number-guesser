fa4bceb7dc803a11526d44c3ed5c6ee84860237a
Philipp Albrecht <philippbalbrecht@gmail.com>
2018-04-11 21:24:47 -0700

    Included the 'PlayAgain' feature

--- app.js
+++ app.js
@@ -8,67 +8,101 @@ Game Functions:
 */
 
 // Game Values
-let min = 1,
-    max = 10,
-    winningNum = 2,
-    guessLeft = 3;
+
+let max  = 10,
+    min = 1,
+    winningNum = randomNum(min, max),
+    guessesLeft = 3;
     
+
+
 // Get the UI elements
-const game = document.querySelector('#game'),
-      minNum = document.querySelector('.min-num'),
-      maxNum = document.querySelector('.max-num'),
-      guessInput = document.querySelector('#guess-input'),
-      guessBtn = document.querySelector('#guess-btn'),
-      message = document.querySelector('.message');
+
+const game = document.querySelector('#game');
+const minNum = document.querySelector('.min-num');
+const maxNum = document.querySelector('.max-num');
+const guessInput = document.querySelector('#guess-input');
+const guessBtn = document.querySelector('#guess-btn');
+const message = document.querySelector('.message');
+
+
+// add play again listner
+
+game.addEventListener('mousedown', function(e){
+    if(e.target.classList.contains('play-again')){
+        location.reload()
+    }
+})
 
 // Assign min and mix
-minNum.textContent = min;
+
 maxNum.textContent = max;
+minNum.textContent = min;
 
 // Listen for guess
 guessBtn.addEventListener('click', function(){
-    let guess = parseInt(guessInput.value);
-    
+    // Parse the guess  
+    const guess = parseInt(guessInput.value);    
     // Validate input
     if (isNaN(guess) || guess < min || guess > max){
-        setMessage(`Please enter a number between ${min} and ${max}.`, 'red')
+        alert('Please enter a number between ' + min + ' and ' + max + '.');
     } else {
          // Game Over - Won
          // Check if won
-    if (guess === winningNum){
-        gameOver(true, `${winningNum} is correct, YOU WIN!`, 'green');
-    } else {
-        // Wrong number
-        guessLeft -= 1;
-        
-        if(guessLeft === 0){
-        // Game over - lost  
-        gameOver(false, 'Game Over, you lost. The correct number was ' + winningNum + '.')
+        if (guess === winningNum){
+            gameOver(true, guess + " is correct! You've WON");
+            
         } else {
-        setMessage(guess + " is not correct. You've got " + guessLeft + " guess let.Try again! ", 'red');
-        // change border color
-        guessInput.style.borderColor = 'red'; 
-
-        }
-      } 
+            // Wrong number
+            guessesLeft -= 1;
+            
+            // Game over - lost 
+            if (guessesLeft === 0){
+                
+             gameOver(false, "You've lost! The correct number was " + winningNum + ".");
+         
+            } else {
+                  // Game continues - wrong answer
+                  // Set message
+                   setMessage(guess + " is incorrect. You've got " + guessesLeft + ".", 'red');
+                  // Set color
+                   guessInput.style.color = 'red';
+                  // Disable input
+                  guessInput.value = '';
+          }
+       }
     }
 });
 
 // Set messsage
 function setMessage(msg, color){
-    message.style.color = color;
     message.textContent = msg;
-}
+    message.style.color = color;
+}        
 
 function gameOver(won, msg){
-        let color;
-        won === true ? color = 'green' : color = 'red'
-        
-        // disable input
-        guessInput.disabled = true;
-        // change border color
-        guessInput.style.borderColor = color;
-        // set message 
-        setMessage(msg, color)
-    
+    let color;
+    won === true ? color = 'green' : color = 'red';
+   // Disable input
+    guessInput.disabled = true;
+    // Set color
+    message.style.color = color;
+    // Set border color 
+    guessInput.style.borderColor = color;
+    // set message
+    setMessage(msg);
+    // Play again?
+    guessBtn.className = 'play-again';
+    // Listen for play again class
+    guessBtn.value = 'Play Again?' 
 }
+
+function randomNum(max, min){
+       return Math.floor(Math.random()*(max - min+1)+min);
+}
+
+
+console.log(winningNum)
+
+
+
